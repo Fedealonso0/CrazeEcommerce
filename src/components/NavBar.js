34f017e3f6b56a2c0a77c0622631b66a2b2efa44
Federico Alonso <fedealonso94@gmail.com>
2022-08-12 13:14:19 -0300

    commit navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+
+function NavBar(){
+    return (
+        <div className="header">
+   <h1 className="headerTitle"><a href="index.js">CryptoStore</a></h1>  
+      <ul className="headerNav">
+        <li className="redirect"><a href="index.js">Home</a></li>
+        <li className="redirect"><a href="products.js">Trending</a></li>
+        <li className="redirect"><a href="foru.js">For You</a></li>
+        <li className="redirect"><a href="shop.js">Shop</a></li>
+    </ul>
+      </div>
+    )
+}
+
+
+export default NavBar;
